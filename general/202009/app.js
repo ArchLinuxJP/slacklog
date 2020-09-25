30ee00e8fdb78953dc7f52ed49cc448ff9eb6d9f
syui <syui@syui.cf>
2020-09-25 23:33:47 +0900

    first

---
+++ general/202009/app.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+    cities: []
+    },
+    methods: {
+        async getCities() {
+        var url = 'https://raw.githubusercontent.com/archlinuxjp/slacklog/master/202009-general.json'
+        await axios.get(url).then(x => { this.cities = x.data })
+        }
+    },
+    mounted() {
+        this.getCities()
+    }
+})
