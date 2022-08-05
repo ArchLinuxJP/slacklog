6276334ae29190a9dd26d53b5b0df8df4414f07b
syui <syui@syui.cf>
2022-08-05 12:30:45 +0900

    fix next pages

--- t.zsh
+++ t.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+d=${0:a:h}/json
 team=archlinuxjp
 repo=slacklog
 general=C0GFH3RHC
@@ -6,13 +7,14 @@ random=C0GFH3RHU
 now=`date '+%Y%m01'`
 date=20191201
 #date=20201201
+limit=200
 
 if [ -f ./config.json ];then
  token=`cat ./config.json|jq -r ".SLACK_TOKEN"`
 fi
 
 test_time(){
-	file=20210101_general.json
+	file=$d/20210101_general.json
 	if [ ! -f $file ];then
 		exit
 	fi
@@ -27,28 +29,59 @@ test_time(){
 }
 
 user_json(){
+	# https://api.slack.com/methods/users.list
 	url=https://slack.com/api/users.list
- curl -X GET -H 'Content-type: application/json' "$url?token=$token" >! user.json
+	if [ ! -f $d/user.json ];then
+ 	curl -X GET -H 'Content-type: application/json' "$url?token=$token" >! $d/user.json
+	fi
+}
+
+slack_history(){
+	if [ ! -f $json.json ];then
+	 curl -X GET -H 'Content-type: application/json' "$url?token=$token&channel=$general&latest=$latest&oldest=$oldest&limit=$limit" |jq . >! $json.json
+	fi
+
+	# next-page
+	# 応答には、値を含む最上位のresponse_metadata属性が含まれnext_cursorます。cursor後続のリクエストでこの値をパラメータとして使用することでlimit、仮想ページごとにコレクション ページをナビゲートできます。
+	if [ -f $json.json ];then
+		n=`cat $json.json|jq -r ".response_metadata.next_cursor"`
+	fi
+	if [ "$n" != "null" ];then
+		i=2
+		while : 
+		do
+		jsons=$json-$i
+		if [ ! -f $jsons.json ];then
+		 curl -X GET -H 'Content-type: application/json' "$url?token=$token&channel=$general&latest=$latest&oldest=$oldest&limit=$limit&cursor=$n" |jq . >! $jsons.json
+		fi
+		if [ -f $jsons.json ];then
+			n=`cat $jsons.json|jq -r ".response_metadata.next_cursor"`
+		fi
+		if [ "$n" = "null" ]; then
+			break
+		fi
+		((i++))
+	done
+	fi
+	unset n
+	unset i
 }
 
 slack_log(){
 while :
 do
+	# https://api.slack.com/methods/conversations.history
 	url=https://slack.com/api/conversations.history
 	oldest=`date -j -f "%Y%m%d" "$date" +%s`
 	latest=`date -j -v+1m -f "%Y%m%d" "$date" +%s`
 	date=`date -r $latest +"%Y%m01"`
 	date_o=`date -r $oldest +"%Y%m01"`
 
-	json=${date_o}_general.json
-	if [ ! -f ./$json ];then
-	 curl -X GET -H 'Content-type: application/json' "$url?token=$token&channel=$general&latest=$latest&oldest=$oldest&include_all_metadata=true" |jq . >! $json
-	fi
+	json=$d/${date_o}_general
+	slack_history
 
-	json=${date_o}_random.json
-	if [ ! -f ./$json ];then
-	 curl -X GET -H 'Content-type: application/json' "$url?token=$token&channel=$random&latest=$latest&oldest=$oldest&include_all_metadata=true" |jq . >! $json
-	fi
+	json=$d/${date_o}_random
+	slack_history
 
  if [ "$now" = "$date" ]; then
   break
@@ -56,5 +89,5 @@ do
 done
 }
 
-#user_json
+user_json
 slack_log
